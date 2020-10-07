5a81ae381826ca8813faea3e3bf235a846158795
Michael Heider <michael.heider@informatik.uni-augsburg.de>
2020-10-08 01:23:43 +0200

    Add StandardScaler to y inputs

--- main/main.py
+++ main/main.py
@@ -2,15 +2,23 @@ from problems import amgauss, make_problem
 from suprb2 import LCS
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 if __name__ == '__main__':
     prob = make_problem("amgauss", 1)
 
-    X, y = prob.generate(25000)
+    X, y = prob.generate(10000)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    y = np.reshape(y, (-1, 1))
 
-    lcs = LCS()
+    scale = StandardScaler()
+
+    scale.fit(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, scale.transform(y))
+
+    lcs = LCS(xdim=(prob.xdim+prob.adim))
 
     lcs.fit(X_train, y_train)
 
