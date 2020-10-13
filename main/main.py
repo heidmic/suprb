ea39dc65f2dddd49b7ff8f74313a082a183fb33e
Michael Heider <michael.heider@informatik.uni-augsburg.de>
2020-10-13 18:56:08 +0200

    Change Problem to a simple 1D polynomial

--- main/main.py
+++ main/main.py
@@ -3,19 +3,35 @@ from suprb2 import LCS
 from suprb2.random_gen import Random
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import numpy as np
 
-if __name__ == '__main__':
-    prob = make_problem("amgauss", 1)
+def f(X):
+    return 0.75*X**3-5*X**2+4*X+12
 
-    X, y = prob.generate(10000)
+
+if __name__ == '__main__':
+    n = 10000
+
+    """prob = make_problem("amgauss", 1)
+
+    X, y = prob.generate(n)
 
     y = np.reshape(y, (-1, 1))
+    
+    xdim = prob.xdim + prob.adim"""
 
-    scale = StandardScaler()
+    X = Random().random.uniform(-2.5, 7, (n, 1))
+    y = f(X)
+    xdim = 1
 
-    scale.fit(y)
+    scale_X = MinMaxScaler(feature_range=(-1, 1))
+    scale_X.fit(X)
+    X = scale_X.transform(X)
+
+    scale_y = StandardScaler()
+    scale_y.fit(y)
+    y = scale_y.transform(y)
 
     X_train, X_test, y_train, y_test = train_test_split(X, scale.transform(y), random_state=Random().split_seed())
 
