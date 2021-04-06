f2784d73f1d816f04d774b039482183ca6fc37ba
RomanSraj <Roman.Sraj@outlook.com>
2021-04-06 16:00:53 +0200

    Add get_classifier_quality

--- suprb2/__init__.py
+++ suprb2/__init__.py
@@ -9,8 +9,9 @@ from suprb2.pool import ClassifierPool
 
 from sklearn.model_selection import train_test_split
 from datetime import datetime
-import mlflow as mf
 from copy import deepcopy
+import mlflow as mf
+import itertools
 
 
 class LCS:
@@ -132,6 +133,20 @@ class LCS:
         #  likely R2
         raise NotImplementedError()
 
+    def get_classifier_quality(self, child):
+        volume = 0
+        cartesian_product = itertools.product(child.upperBounds, child.lowerBounds)
+        for p in cartesian_product:
+            volume += abs(p[0] - p[1])
+
+        # TODO is this correct or shall the quality drop to 0 since upper and lower bounds are the same?
+        if volume == 0:
+            volume = 1
+
+        quality = child.error / (volume * Config().rule_discovery["quality_parameter"])
+
+        return quality
+
     def discover_rules(self, X, y):
         # draw n examples from data
         idxs = Random().random.choice(np.arange(len(X)),
@@ -149,7 +164,7 @@ class LCS:
                     children.append(child)
                 # ToDo instead of greedily taking the minimum, treating all
                 #  below a certain threshhold as equal might yield better models
-                cl = children[np.argmin([child.error for child in children])]
+                cl = children[np.argmin([self.get_classifier_quality(child) for child in children])]
             if cl.error < self.default_error(y[np.nonzero(cl.matches(X))]):
                 ClassifierPool().classifiers.append(cl)
 
--- suprb2/config.py
+++ suprb2/config.py
@@ -6,34 +6,33 @@ class Config:
     See https://code.activestate.com/recipes/66531-singleton-we-dont-need-no-stinkin-singleton-the-bo/ .
     """
     __shared_state = {
-                      "n_elitists": 1,
-                      "rule_discovery": {
-                          "name": '(1+lambda)-ES',
-                          "cl_min_range": 0.2,
-                          "nrules": 50,
-                          "lmbd": 20,  # not allowed to use lambda
-                          "sigma": 0.2,
-                          "steps_per_step": 10
-                      },
-                      "solution_creation": {
-                          "name": '(1+1)-ES',
-                          # "pop_size": 1,
-                          # "crossover_type": None,
-                          "mutation_rate": 0.2,
-                          "fitness": "pseudo-BIC",
-                          "steps_per_step": 1000
-                      },
-                      "initial_pool_size": 50,
-                      "initial_genome_length": 100000,
-                      "steps": 250,
-                      "xdim": None,
-                      "use_validation": False,
-                      "default_prediction": None,
-                      "var": None,
-                      "logging": True
+        "n_elitists": 1,
+        "rule_discovery": {
+            "name": '(1+lambda)-ES',
+            "cl_min_range": 0.2,
+            "nrules": 50,
+            "lmbd": 20,  # not allowed to use lambda
+            "sigma": 0.2,
+            "steps_per_step": 10,
+            "quality_parameter": 1
+        },
+        "solution_creation": {
+            "name": '(1+1)-ES',
+            # "pop_size": 1,
+            # "crossover_type": None,
+            "mutation_rate": 0.2,
+            "fitness": "pseudo-BIC",
+            "steps_per_step": 1000
+        },
+        "initial_pool_size": 50,
+        "initial_genome_length": 100000,
+        "steps": 250,
+        "xdim": None,
+        "use_validation": False,
+        "default_prediction": None,
+        "var": None,
+        "logging": True
     }
 
     def __init__(self):
         self.__dict__ = self.__shared_state
-
-
