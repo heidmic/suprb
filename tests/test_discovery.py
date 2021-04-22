39dfcbf92207d335af6ae9dd96b4fde831b9dc2f
kFischerG <klaus.fischer@conlance.de>
2021-04-22 19:27:57 +0200

    Refactor RuleDicoverer
    - Fixed the step method
    - Removed useless methods
    - Added more tests

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,6 +1,7 @@
-from tests.test_support import TestSupport
+from suprb2.config import Config
 from suprb2.pool import ClassifierPool
 from suprb2.classifier import Classifier
+from tests.test_support import TestSupport
 from suprb2.discovery import RuleDiscoverer
 
 import unittest
@@ -17,62 +18,123 @@ class TestDiscovery(unittest.TestCase):
         Resets the Classifier Pool for the next test.
         """
         ClassifierPool().classifiers = list()
+        Config().__init__()
 
     # ------------- step() --------------
 
 
-    def test_step_mu_equals_lambda(self):
+    def test_step_mu_equals_lambda_comma(self):
         """
         Tests the method RuleDiscoverer.step().
 
-        Test that when mu is equal to lambda, then the population
-        will not grow nor sink.
-        Classifier will only be added if their error is acceptable,
-        that is why all errors are mocked with -1.
+        With seed = 1, just one of the children have
+        errors good enough to be added to the pool.
+        Since we are using the ',' replacement, this
+        lonly child will replace their parents, and
+        we will have a population of 1
         """
         mu, lmbd = (10, 10)
-        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, selection='+', steps_per_step=1)
+        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, replacement=',', steps_per_step=1)
         X, y = TestSupport.initiate_pool(mu, 1)
 
         optimizer = RuleDiscoverer()
         optimizer.step(X, y)
-        self.assertEqual(len(ClassifierPool().classifiers), mu)
+        self.assertEqual(len(ClassifierPool().classifiers), 1)
 
 
-    def test_step_mu_bigger_than_lambda(self):
+    def test_step_mu_equals_lambda_plus(self):
         """
         Tests the method RuleDiscoverer.step().
 
-        Test that when mu is bigger than lambda, then the population
-        will shrink.
-        Classifier will only be added if their error is acceptable,
-        that is why all errors are mocked with -1.
+        With seed = 2, just 2 of the children have
+        errors good enough to be added to the pool.
+        Since we are using the '+' replacement, this
+        lonly child will be added with their parents
+        to the next generation (with size 10 + 2 = 12)
+        """
+        mu, lmbd = (10, 10)
+        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, replacement='+', steps_per_step=1)
+        X, y = TestSupport.initiate_pool(mu, 2)
+
+        optimizer = RuleDiscoverer()
+        optimizer.step(X, y)
+        self.assertEqual(len(ClassifierPool().classifiers), 12)
+
+
+    def test_step_mu_bigger_than_lambda_comma(self):
+        """
+        Tests the method RuleDiscoverer.step().
+
+        With seed = 1, just one of the children have
+        errors good enough to be added to the pool.
+        Since we are using the ',' replacement, this
+        lonly child will replace their parents, and
+        we will have a population of 1
         """
         mu, lmbd = (15, 10)
-        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, selection='+', steps_per_step=1)
+        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, replacement=',', steps_per_step=1)
         X, y = TestSupport.initiate_pool(mu, 1)
 
         optimizer = RuleDiscoverer()
         optimizer.step(X, y)
-        self.assertLess(len(ClassifierPool().classifiers), mu)
+        self.assertEqual(len(ClassifierPool().classifiers), 1)
 
 
-    def test_step_mu_smaller_than_lambda(self):
+    def test_step_mu_bigger_than_lambda_plus(self):
         """
         Tests the method RuleDiscoverer.step().
 
-        Test that when mu is equal to lambda, then the population
-        will grow.
-        Classifier will only be added if their error is acceptable,
-        that is why all errors are mocked with -1.
+        With seed = 1, just one of the children have
+        errors good enough to be added to the pool.
+        Since we are using the '+' replacement, this
+        lonly child will be added with their parents
+        to the next generation (with size 15 + 1 = 16)
         """
-        mu, lmbd = (10, 15)
-        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, selection='+', steps_per_step=1)
+        mu, lmbd = (15, 10)
+        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, replacement='+', steps_per_step=1)
         X, y = TestSupport.initiate_pool(mu, 1)
 
         optimizer = RuleDiscoverer()
         optimizer.step(X, y)
-        self.assertGreaterEqual(len(ClassifierPool().classifiers), mu)
+        self.assertEqual(len(ClassifierPool().classifiers), 16)
+
+
+    def test_step_mu_smaller_than_lambda_comma(self):
+        """
+        Tests the method RuleDiscoverer.step().
+
+        With seed = 2, just one of the children have
+        errors good enough to be added to the pool.
+        Since we are using the ',' replacement, this
+        lonly child will replace their parents, and
+        we will have a population of 1
+        """
+        mu, lmbd = (10, 15)
+        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, replacement=',', steps_per_step=1)
+        X, y = TestSupport.initiate_pool(mu, 2)
+
+        optimizer = RuleDiscoverer()
+        optimizer.step(X, y)
+        self.assertGreaterEqual(len(ClassifierPool().classifiers), 1)
+
+
+    def test_step_mu_smaller_than_lambda_plus(self):
+        """
+        Tests the method RuleDiscoverer.step().
+
+        With seed = 1, just one of the children have
+        errors good enough to be added to the pool.
+        Since we are using the '+' replacement, this
+        lonly child will be added with their parents
+        to the next generation (with size 10 + 1 = 11)
+        """
+        mu, lmbd = (10, 15)
+        TestSupport.set_rule_discovery_configs(mu=mu, lmbd=lmbd, replacement='+', steps_per_step=1)
+        X, y = TestSupport.initiate_pool(mu, 1)
+
+        optimizer = RuleDiscoverer()
+        optimizer.step(X, y)
+        self.assertEqual(len(ClassifierPool().classifiers), 11)
 
 
     # ------------- remove_parents_from_pool() --------------
@@ -105,12 +167,13 @@ class TestDiscovery(unittest.TestCase):
         Checks if the average of the classifiers' boundaries
         is propperly calculated.
 
-        child.lowerBound = average(parents.lowerBounds)
-        child.upperBound = average(parents.upperBounds)
+        child_1.lowerBound = average(random_vater.lowerBound, random_mother.lowerBound)
+        child_1.upperBound = average(random_vater.upperBound, random_mother.upperBound)
         """
         TestSupport.set_rule_discovery_configs(recombination='intermediate')
-        child = RuleDiscoverer().recombine(TestSupport.mock_classifiers(10))
-        self.assertEquals((child.lowerBounds, child.upperBounds), (5, 5))
+        child = RuleDiscoverer().recombine(TestSupport.mock_specific_classifiers([ [2, 2, 0], [4, 2, 0], [2, 4, 0], [4, 4, 0] ]))[0]
+        self.assertIn(child.lowerBounds, [2, 3, 4])
+        self.assertIn(child.upperBounds, [2, 3, 4])
 
 
     def test_recombine_default_strategy(self):
@@ -124,8 +187,26 @@ class TestDiscovery(unittest.TestCase):
         Classifier.
         """
         parents = TestSupport.mock_classifiers(10)
-        child = RuleDiscoverer().recombine(parents)
+        child = RuleDiscoverer().recombine(parents)[0]
         self.assertIsInstance(child, Classifier)
+
+
+    # ------------- mutate_and_fit() --------------
+
+
+    def test_mutate_and_fit(self):
+        """
+        Tests the method RuleDiscoverer.mutate_and_fit().
+
+        Checks if the lower and upper bounds were slightly
+        ( <= (u - l) / 10 ) changed and if there is an
+        error present.
+        """
+        n = 2
+        classifiers = TestSupport.mock_specific_classifiers([ [[5], [10], None], [[10], [5], None] ])
+        X, y = TestSupport.generate_input(n)
+        RuleDiscoverer().mutate_and_fit(classifiers, X, y)
+        self.assertLessEqual(classifiers[0].lowerBounds, 5 + (10 - 5 / 10))
 
 
     # ------------- replace() --------------
@@ -146,89 +227,6 @@ class TestDiscovery(unittest.TestCase):
         array_concatenation = np.concatenate((children, parents))
         replacement_array = RuleDiscoverer().replace(parents, children)
         self.assertIsNone(np.testing.assert_array_equal(replacement_array, array_concatenation))
-
-
-    # ------------- filter_classifiers() --------------
-
-
-    def test_filter_classifiers_in_pool(self):
-        """
-        Tests the method RuleDiscoverer.filter_classifiers().
-
-        Checks that all classifiers filtered have an error
-        smaller than the default one.
-        """
-        X, y = TestSupport.initiate_pool(10, 1)
-
-        optimizer = RuleDiscoverer()
-        filtered_cls = optimizer.filter_classifiers(np.array(ClassifierPool().classifiers), X, y)
-        for cl in filtered_cls:
-            self.assertTrue(cl.error < optimizer.default_error(y))
-
-
-    def test_filter_classifiers_that_were_filtered(self):
-        """
-        Tests the method RuleDiscoverer.filter_classifiers().
-
-        Checks that all classfiers that are not in the
-        filtered group have either no error (error == None)
-        or error is bigger than the default error.
-        """
-        X, y = TestSupport.initiate_pool(10, 1)
-
-        optimizer = RuleDiscoverer()
-        filtered_cls = optimizer.filter_classifiers(np.array(ClassifierPool().classifiers), X, y)
-        for cl in ClassifierPool().classifiers:
-            if cl not in filtered_cls:
-                self.assertTrue(cl.error is None or cl.error > optimizer.default_error(y))
-
-
-    # ------------- best_lambda_classifiers() --------------
-
-
-    def test_best_lambda_classifiers_sorted(self):
-        """
-        Tests the method RuleDiscoverer.best_lambda_classifiers().
-
-        Checks that the lambda best classifiers are sorted
-        ascending acoording to their errors.
-        """
-        lmbd = 5
-        candidates = TestSupport.mock_classifiers(10, errors=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        best_lambda_cls = RuleDiscoverer().best_lambda_classifiers(candidates, lmbd)
-
-        self.assertEquals(len(best_lambda_cls), lmbd)
-        self.assertTrue(all(best_lambda_cls[i].error <= best_lambda_cls[i + 1].error for i in range(lmbd - 1)))
-
-
-    def test_best_lambda_classifiers_lowest_errors(self):
-        """
-        Tests the method RuleDiscoverer.best_lambda_classifiers().
-
-        Checks that the other classifiers have bigger
-        errors than any of the lambda classifiers.
-        """
-        lmbd = 5
-        candidates = TestSupport.mock_classifiers(10, errors=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        best_lambda_cls = RuleDiscoverer().best_lambda_classifiers(candidates, lmbd)
-
-        for cl in candidates:
-            if cl not in best_lambda_cls:
-                self.assertTrue(cl.error > best_lambda_cls[-1].error)
-
-
-    def test_best_lambda_classifiers_candidates_smaller_than_lambda(self):
-        """
-        Tests the method RuleDiscoverer.best_lambda_classifiers().
-
-        If candidates.size < lmbd, then lmbd = candidates.size.
-        """
-        lmbd = 10
-        candidates = TestSupport.mock_classifiers(5, errors=[0, 1, 2, 3, 4])
-        old_size = len(candidates)
-        best_lambda_cls = RuleDiscoverer().best_lambda_classifiers(candidates, lmbd)
-
-        self.assertEquals(old_size, len(best_lambda_cls))
 
 
 if __name__ == '__main__':
