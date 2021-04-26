8e5c344edb08b612a671ced9dad558f9cf76fd3b
m.heider <michael.heider@informatik.uni-augsburg.de>
2021-04-26 17:57:04 +0200

    Expand logging

--- main.py
+++ main.py
@@ -141,10 +141,10 @@ if __name__ == '__main__':
             error = mean_squared_error(y_test, y_pred)
 
             if True:
-                plot_results(X_test, y_test, y_pred, lcs.get_elitist(), seed)
+                plot_results(X_test, y_test, y_pred, lcs.get_elitist(), saveas=seed)
                 plot_perfrecords(lcs.perf_recording.__dict__,
-                                 ["elitist_complexity", "elitist_val_error"], seed)
-                plot_error_complexity(lcs.perf_recording, seed)
+                                 ["elitist_complexity", "elitist_val_error"], saveas=seed)
+                plot_error_complexity(lcs.perf_recording, saveas=seed)
 
             mf.log_metric("RMSE", np.sqrt(error))
             print(f"Finished at {datetime.now().time()}. RMSE was {np.sqrt(error)}")
