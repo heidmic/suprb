f3df3250a757b7472267ae9fe136137db1720013
RomanSraj <Roman.Sraj@outlook.com>
2021-05-28 14:56:37 +0200

    Refactor Fitness class

--- suprb2/fitness.py
+++ suprb2/fitness.py
@@ -24,26 +24,25 @@ class Fitness:
             self.fitness_function = None
             exit()
 
-    def determine_fitness(self, X_val, y_val, predicted_X_val, parameters, classifiers):
-        return self.fitness_function(X_val, y_val, predicted_X_val, parameters, classifiers)
-
-    def pseudo_bic(self, X_val, y_val, predicted_X_val, parameters, classifiers):
-        n = len(X_val)
-
+    def calculate_bic_error(self, n, y_val, predicted_X_val):
         # mse = ResidualSumOfSquares / NumberOfSamples
         self.error = np.sum(np.square(y_val - predicted_X_val)) / n
 
+    def calculate_bic_fitness(self, n, parameters):
         # BIC -(n * np.log(rss / n) + complexity * np.log(n))
         self.fitness = - (n * np.log(self.error) + parameters * np.log(n))
 
+    def pseudo_bic(self, X_val, y_val, predicted_X_val, parameters, classifiers):
+        n = len(X_val)
+        self.calculate_bic_error(n, y_val, predicted_X_val)
+        self.calculate_bic_fitness(n, parameters)
+
     def bic_matching_punishment(self, X_val, y_val, predicted_X_val, parameters, classifiers):
         n = len(X_val)
-        # mse = ResidualSumOfSquares / NumberOfSamples
-        self.error = np.sum(np.square(y_val - predicted_X_val)) / n
-
         matching_pun = np.sum(np.nonzero(np.sum(np.array([cl.matches(X_val) for cl in classifiers]), 1) > 1))
-        # BIC -(n * np.log(rss / n) + complexity * np.log(n))
-        self.fitness = - (n * np.log(self.error) + (parameters + matching_pun) * np.log(n))
+        
+        self.calculate_bic_error(n, y_val, predicted_X_val)
+        self.calculate_bic_fitness(n, parameters + matching_pun)
 
     def mse(self, X_val, y_val, predicted_X_val, parameters, classifiers):
         self.error = mean_squared_error(y_val, predicted_X_val)
@@ -54,3 +53,6 @@ class Fitness:
 
         constant = len(classifiers) - Config().ind_size if len(classifiers) > Config().ind_size else 0
         self.fitness -= constant
+
+    def determine_fitness(self, X_val, y_val, predicted_X_val, parameters, classifiers):
+        self.fitness_function(X_val, y_val, predicted_X_val, parameters, classifiers)
