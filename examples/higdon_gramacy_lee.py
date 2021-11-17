e4d1bb48406ccc34989aed7dc6d2185eecb35505
Saethox <joni.htruw@gmail.com>
2021-11-17 11:13:25 +0100

    Make Higdon-Gramacy-Lee example reproducible

--- examples/higdon_gramacy_lee.py
+++ examples/higdon_gramacy_lee.py
@@ -8,17 +8,19 @@ from suprb2 import SupRB2
 from suprb2.optimizer import rule, individual
 from suprb2.optimizer.individual import ga
 from suprb2.optimizer.rule import es
+from suprb2.utils import check_random_state
 
 sns.set_theme(style='darkgrid')
 sns.set(rc={'figure.figsize': (9, 9)})
 
 
-def load_higdon_gramacy_lee(n_samples=1000, noise=0.):
+def load_higdon_gramacy_lee(n_samples=1000, noise=0., random_state=None):
+    random_state_ = check_random_state(random_state)
     X = np.linspace(0, 20, num=n_samples)
     y = np.zeros(n_samples)
     y[X < 10] = np.sin(np.pi * X[X < 10] / 5) + 0.2 * np.cos(4 * np.pi * X[X < 10] / 5)
     y[X >= 10] = X[X >= 10] / 10 - 1
-    y += np.random.normal(scale=noise, size=n_samples)
+    y += random_state_.normal(scale=noise, size=n_samples)
     X = X.reshape((-1, 1))
     return X, y
 
@@ -42,9 +44,10 @@ if __name__ == '__main__':
             n_iter=32,
             crossover=individual.ga.crossover.Uniform(),
         ),
-        n_iter=8,
+        n_iter=16,
         n_rules=4,
         progress_bar=True,
+        random_state=random_state,
     )
 
     scores = cross_validate(model, X, y, cv=9, n_jobs=1, verbose=10, scoring=['r2', 'neg_mean_squared_error'],
