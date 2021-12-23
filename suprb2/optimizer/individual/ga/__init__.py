942d06113953f3b8b833b85e500990f24ba1c75f
Saethox <joni.htruw@gmail.com>
2021-12-23 20:55:20 +0100

    Change GA file format to be consistent with the rest

--- suprb2/optimizer/individual/ga/__init__.py
+++ suprb2/optimizer/individual/ga/__init__.py
@@ -1,4 +1,4 @@
-from .ga import GeneticAlgorithm
+from .base import GeneticAlgorithm
 from .crossover import IndividualCrossover
 from .mutation import IndividualMutation
 from .selection import IndividualSelection
