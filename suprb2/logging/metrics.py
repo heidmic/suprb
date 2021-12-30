ee2317ee31f6ec9cd454820f68c031b3652c0653
Saethox <joni.htruw@gmail.com>
2021-12-30 17:30:19 +0100

    Fix hamming distance and diversity implementation

--- suprb2/logging/metrics.py
+++ suprb2/logging/metrics.py
@@ -6,12 +6,11 @@ from suprb2 import Rule, Individual
 
 def genome_diversity(population: list[Individual]):
     combinations = list(itertools.combinations(population, 2))
-    return sum([hamming_distance(com[0].genome, com[1].genome) for com in combinations])
+    return sum([hamming_distance(a.genome, b.genome) for a, b in combinations])
 
 
 def hamming_distance(a: np.ndarray, b: np.ndarray):
-    r = (1 << np.arange(8))[:, None]
-    return np.count_nonzero((np.bitwise_xor(a, b) & r) != 0)
+    return np.count_nonzero(a ^ b)
 
 
 def matched_training_samples(pool: list[Rule]):
