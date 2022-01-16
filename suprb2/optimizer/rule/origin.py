68cee398856aee2011462bd6a0f84ac567cc9028
Saethox <joni.htruw@gmail.com>
2022-01-16 23:56:20 +0100

    Rename new origin components and add extra delay parameter

--- suprb2/optimizer/rule/origin.py
+++ suprb2/optimizer/rule/origin.py
@@ -18,21 +18,28 @@ class RuleOriginGeneration(BaseComponent, metaclass=ABCMeta):
         pass
 
 
-class UniformOrigin(RuleOriginGeneration):
+class UniformInputOrigin(RuleOriginGeneration):
     """Sample origins uniformly in the input space."""
 
     def __call__(self, n_rules: int, X: np.ndarray, random_state: RandomState, **kwargs) -> np.ndarray:
         return random_state.uniform(np.min(X, axis=0), np.max(X, axis=0), size=(n_rules, X.shape[1]))
 
 
-class RuleStatisticsOrigin(RuleOriginGeneration):
+class UniformSamplesOrigin(RuleOriginGeneration):
+    """Sample origins uniformly in the sample space."""
+
+    def __call__(self, n_rules: int, X: np.ndarray, random_state: RandomState, **kwargs) -> np.ndarray:
+        return random_state.choice(X, axis=0, size=n_rules)
+
+
+class RouletteWheelOrigin(RuleOriginGeneration):
     """
     Sample origins with weights calculated from rules in the pool.
     If `use_elitist` is set, the matching is only calculated on
      the subpopulation of the current elitist (the current elitist solution).
     """
 
-    def __init__(self, use_elitist: bool = False):
+    def __init__(self, use_elitist: bool = True):
         self.use_elitist = use_elitist
 
     def __call__(self, n_rules: int, X: np.ndarray, pool: list[Rule], elitist: Optional[Individual],
@@ -41,7 +48,8 @@ class RuleStatisticsOrigin(RuleOriginGeneration):
         subgroup = elitist.subpopulation if elitist is not None and self.use_elitist else pool
 
         if subgroup:
-            weights = self._calculate_weights(subgroup=subgroup, X=X, elitist=elitist, random_state=random_state, **kwargs)
+            weights = self._calculate_weights(subgroup=subgroup, X=X, elitist=elitist, random_state=random_state,
+                                              **kwargs)
             weights_sum = np.sum(weights)
             # If all weights are zero, no bias is needed
             probabilities = weights / weights_sum if weights_sum != 0 else None
@@ -57,18 +65,15 @@ class RuleStatisticsOrigin(RuleOriginGeneration):
         pass
 
 
-class RuleMatching(RuleStatisticsOrigin):
-    """
-    Bias the examples that were matched less than others by rules
-     to have a higher probability to be selected.
-    """
+class Matching(RouletteWheelOrigin):
+    """Bias the examples that were matched less than others by rules to have a higher probability to be selected."""
 
     def _calculate_weights(self, subgroup: list[Rule], **kwargs) -> np.ndarray:
         return np.count_nonzero(np.stack([rule.match_ for rule in subgroup], axis=0) == 0, axis=0)
 
 
-class RuleError(RuleStatisticsOrigin):
-    """Bias the examples that have higher error on rules to have a higher probability to be selected."""
+class SquaredError(RouletteWheelOrigin):
+    """Bias the examples that have higher squared error on rules to have a higher probability to be selected."""
 
     def _calculate_weights(self, X: np.ndarray = None, y: np.ndarray = None, elitist: Individual = None,
                            **kwargs) -> np.ndarray:
