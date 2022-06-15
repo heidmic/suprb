b6aaeaf116abb266584ad38d8ff40c706912c4d7
hubanton <anton.huber98@yahoo.com>
2022-06-15 10:43:18 +0200

    Adjust logging

--- suprb/logging/default.py
+++ suprb/logging/default.py
@@ -12,6 +12,8 @@ class DefaultLogger(BaseLogger):
 
     params_: dict
     metrics_: dict
+    genomes_: dict
+    pool_: dict
 
     def log_param(self, key, value):
         self.params_[key] = value
@@ -26,7 +28,7 @@ class DefaultLogger(BaseLogger):
     def log_init(self, X: np.ndarray, y: np.ndarray, estimator: SupRB):
         self.params_ = {}
         self.metrics_ = defaultdict(dict)
-
+        self.genomes_ = {}
         self.log_params(**estimator.get_params())
 
     def log_iteration(self, X: np.ndarray, y: np.ndarray, estimator: SupRB, iteration: int):
@@ -64,4 +66,60 @@ class DefaultLogger(BaseLogger):
         log_metric("training_score", elitist.score(X, y))
 
     def log_final(self, X: np.ndarray, y: np.ndarray, estimator: SupRB):
-        pass
+        def log_metric(key, value):
+            self.log_metric(key=key, value=value, step=estimator.step_)
+
+        def log_metric_min_max_mean(metric_name: str, lst: list):
+            log_metric(metric_name + '_min', min(lst))
+            log_metric(metric_name + '_mean', sum(lst) / len(lst))
+            log_metric(metric_name + '_max', max(lst))
+
+        def parse_genome(genome_lst):
+            str_genomes = []
+            for x in genome_lst:
+                str_genomes.append(''.join((str(int(e))) for e in x))
+            return str_genomes
+
+        def get_convergence(lst: list, threshold: int):
+            copied_lst = lst.copy()
+            final_genome = copied_lst.pop()
+            length_final = final_genome.shape[0]
+            for i, gen in zip(range(len(copied_lst)), reversed(copied_lst)):
+                thresh = 0
+                length_current = gen.shape[0]
+                for x in range(length_final):
+                    if x > length_current - 1:
+                        if final_genome[x] != 0:
+                            thresh += 1
+                    elif final_genome[x] != gen[x]:
+                        thresh += 1
+                    if thresh > threshold:
+                        return i
+            return len(lst) - 1
+
+        def get_fit_convergence(lst: list, threshold: float):
+            copied_lst = lst.copy()
+            final_fitness = copied_lst.pop()
+            thresh = 1 + threshold
+            for i, fitness_ in zip(range(len(copied_lst)), reversed(copied_lst)):
+                if final_fitness > (fitness_ * thresh):
+                    return i
+            return len(copied_lst)
+
+        # Log delay
+        log_metric_min_max_mean("delay", estimator.final_iterations)
+
+        # Log elitist convergence
+        genomes = estimator.global_genomes
+        str_genomes = parse_genome(genomes)
+        genome_dict = {i: str_genomes[i] for i in range(0, len(str_genomes))}
+        self.genomes_ = genome_dict
+        log_metric("elitist_convergence_thresh_0", get_convergence(genomes, 0))
+        log_metric("elitist_convergence_thresh_1", get_convergence(genomes, 1))
+        log_metric("elitist_convergence_thresh_2", get_convergence(genomes, 2))
+
+        # Log fitness convergence
+        log_metric("fit_convergence_0", get_fit_convergence(estimator.fitness_iter, 0.01))
+        log_metric("fit_convergence_1", get_fit_convergence(estimator.fitness_iter, 0.001))
+        log_metric("fit_convergence_2", get_fit_convergence(estimator.fitness_iter, 0.0001))
+        log_metric("fit_convergence_3", get_fit_convergence(estimator.fitness_iter, 0.00001))
