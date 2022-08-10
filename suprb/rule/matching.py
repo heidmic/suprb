3a6ff3173060eff2784a85d3798e0646461abde1
Tommy <thomasniemeier99@gmail.com>
2022-08-10 11:47:40 +0200

    Add GaussianKernelFunction as matching function for hyperellipsoidal conditions
    implemented the gaussiankernel function and volume of n-dim ellipsoid

--- suprb/rule/matching.py
+++ suprb/rule/matching.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 
 import numpy as np
+import math
 
 from suprb.base import BaseComponent
 
@@ -46,8 +47,7 @@ class OrderedBound(MatchingFunction):
         self.bounds = bounds
 
     def __call__(self, X: np.ndarray):
-        return np.all((self.bounds[:, 0] <= X) &
-                      (X <= self.bounds[:, 1]), axis=1)
+        return np.all((self.bounds[:, 0] <= X) & (X <= self.bounds[:, 1]), axis=1)
 
     @property
     def volume_(self):
@@ -75,6 +75,58 @@ class OrderedBound(MatchingFunction):
         diff = self.bounds[:, 1] - self.bounds[:, 0]
         if min_range > 0:
             invalid_indices = np.argwhere(diff < min_range)
+
             self.bounds[invalid_indices, 0] -= min_range / 2
             self.bounds[invalid_indices, 1] += min_range / 2
 
+
+class GaussianKernelFunction(MatchingFunction):
+    """
+    A standard kernel-based matching function producing multi-dimensional
+    hyperellipsoidal conditions. In effect, a center point (c) and deviations (d)
+    are specified for each dimension. A threshhold (t) defines if a rule is matched or not.
+    An example x is matched iff exp((x_i - c_i)^2 / 2*(d_i^2)) > t for all dimensions i
+    """
+    def __init__(self, parameters: np.ndarray, threshold: float = 0.7):
+        self.center = parameters[:, 0]
+        self.deviations = parameters[:, 1]
+        self.threshold = threshold
+
+    def __call__(self, X: np.ndarray):
+        return np.exp(np.sum(((X - self.center) ** 2) / (2 * (self.deviations ** 2)), axis=1) * -1) > self.threshold
+
+    @property
+    def volume_(self):
+        """
+        Calculates the volume of the ellipsoid.
+        Reference how to calculate the volume of an n-dim ellipsoid:
+        https://analyticphysics.com/Higher%20Dimensions/Ellipsoids%20in%20Higher%20Dimensions.htm
+        """
+
+        dim = self.center.shape
+        pre_factor = (2 * (np.pi ** (dim / 2))) / (dim * math.gamma(dim / 2))
+        prod_deviations = np.prod(self.deviations)
+
+        return pre_factor * prod_deviations
+
+    def copy(self):
+        return GaussianKernelFunction(parameters=np.stack((self.center, self.deviations), axis=1),
+                                      threshold=self.threshold)
+
+    def _validate_bounds(self, X: np.ndarray):
+        """Validates that bounds have the correct shape."""
+        parameters = np.stack((self.center, self.deviations), axis=1)
+
+        if parameters.shape[1] != 2:
+            raise ValueError(f"specified bounds are not of shape (-1, 2), but {parameters.shape}")
+
+        if parameters.shape[0] != X.shape[1]:
+            raise ValueError(f"bounds- and input data dimension mismatch: {parameters.shape[0]} != {X.shape[1]}")
+
+    def clip(self, bounds: np.ndarray):
+        #TODO: implement
+        pass
+
+    def min_range(self, min_range: float):
+        #TODO: implement
+        pass
