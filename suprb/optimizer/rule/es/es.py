4d1e7ab7d02c1df8a79408f1bb96906b99f68bfd
Roman Sraj <Roman.Sraj@outlook.com>
2022-10-13 14:09:55 +0200

    Remove print

--- suprb/optimizer/rule/es/es.py
+++ suprb/optimizer/rule/es/es.py
@@ -123,6 +123,5 @@ class ES1xLambda(ParallelSingleRuleGeneration):
                     break
 
             mutation.adapt(elitist.fitness_)
-            print(iteration, type(mutation.operator_), elitist.fitness_)
 
         return elitist
