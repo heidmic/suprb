d932d0f2269537255e39004d16ae00822560dce5
Roman Sraj <Roman.Sraj@outlook.com>
2023-06-01 12:03:20 +0200

    Add experience cap

--- suprb/solution/mixing_model.py
+++ suprb/solution/mixing_model.py
@@ -34,6 +34,27 @@ class RouletteWheel(FilterSubpopulation):
         return self.random_state.choice(subpopulation, p=weights, size=self.rule_amount, replace=False)
 
 
+class ExperienceCalculation():
+    def __init__(self, lower_bound: float = -np.inf, upper_bound: float = np.inf):
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+    def __call__(self, subpopulation: list[Rule], dim: int = None):
+        return np.array([rule.experience_ for rule in subpopulation])
+
+
+class CapExperience(ExperienceCalculation):
+    def __call__(self, subpopulation: list[Rule], dim: int = None):
+        experiences = np.array([rule.experience_ for rule in subpopulation])
+        return np.clip(experiences, self.lower_bound, self.upper_bound)
+
+
+class CapExperienceWithDimensionality(ExperienceCalculation):
+    def __call__(self, subpopulation: list[Rule], dim: int = None):
+        experiences = np.array([rule.experience_ for rule in subpopulation])
+        return np.clip(experiences, self.lower_bound * dim, self.upper_bound * dim)
+
+
 class ErrorExperienceHeuristic(MixingModel):
     """
     Performs mixing similar to the Inverse Variance Heuristic from
@@ -41,9 +62,11 @@ class ErrorExperienceHeuristic(MixingModel):
     but using (error / experience) as a mixing function.
     """
 
-    def __init__(self, filter_subpopulation: FilterSubpopulation = None):
+    def __init__(self, filter_subpopulation: FilterSubpopulation = None, experience_calculation: ExperienceCalculation = None, experience_weight: float = 1):
         self.input_size = None
         self.filter_subpopulation = filter_subpopulation
+        self.experience_calculation = experience_calculation
+        self.experience_weight = experience_weight
 
     def __call__(self, X: np.ndarray, subpopulation: list[Rule], cache=False) -> np.ndarray:
         self.input_size = X.shape[0]
@@ -55,7 +78,7 @@ class ErrorExperienceHeuristic(MixingModel):
         subpopulation = list(self.filter_subpopulation(subpopulation))
 
         local_pred, matches = self._get_local_pred(X, subpopulation, cache)
-        taus = self._get_taus(subpopulation)
+        taus = self._get_taus(subpopulation, X.shape[1])
 
         # Stack all local predictions and sum them weighted with tau
         pred = np.sum(local_pred * taus[:, None], axis=0)
@@ -83,12 +106,12 @@ class ErrorExperienceHeuristic(MixingModel):
 
         return local_pred, matches
 
-    def _get_taus(self, subpopulation: list[Rule]):
+    def _get_taus(self, subpopulation: list[Rule], dim: int):
         # Get errors and experience of all rules in subpopulation
-        experiences = np.array([rule.experience_ for rule in subpopulation])
+        experiences = self.experience_calculation(subpopulation, dim)
         errors = np.array([rule.error_ for rule in subpopulation])
 
-        return (1 / errors) * experiences
+        return (1 / errors) * (experiences * self.experience_weight)
 
     def _get_tau_sum(self, subpopulation: list[Rule], matches: list[Rule], taus: list[int]):
         # Sum all taus
--- tests/test_mixing_model.py
+++ tests/test_mixing_model.py
@@ -4,8 +4,8 @@ import numpy as np
 from suprb import SupRB
 from suprb.utils import check_random_state
 from suprb.rule import Rule
-from suprb.rule.matching import OrderedBound
 from suprb.rule.fitness import VolumeWu
+from suprb.rule.matching import OrderedBound
 
 import suprb.solution.mixing_model as mixing_model
 
@@ -29,20 +29,21 @@ class TestMixingModel(unittest.TestCase):
         random_state = check_random_state(42)
 
         for i in range(100):
-            subpopulation.append(self.create_rule(i/100, 1, 0.01))
+            subpopulation.append(self.create_rule(i/100, i, 0.01))
 
         random_state.shuffle(subpopulation)
 
         return subpopulation
 
-    def test_NBestFitness_filter_subpopulation(self):
+    def setUp(self):
+        self.subpopulation = self.create_subpopulation()
+
+    def test_NBestFitness(self):
         rule_amount = 4
         random_state = 42
 
-        subpopulation = self.create_subpopulation()
-
         mixing = mixing_model.NBestFitness(rule_amount, random_state)
-        result = mixing(subpopulation)
+        result = mixing(self.subpopulation)
 
         self.assertEqual(len(result), 4)
         self.assertEqual(result[0].fitness_, 0.96)
@@ -50,14 +51,12 @@ class TestMixingModel(unittest.TestCase):
         self.assertEqual(result[2].fitness_, 0.98)
         self.assertEqual(result[3].fitness_, 0.99)
 
-    def test_NRandom_filter_subpopulation(self):
+    def test_NRandom(self):
         rule_amount = 4
         random_state = 42
 
-        subpopulation = self.create_subpopulation()
-
         mixing = mixing_model.NRandom(rule_amount, random_state)
-        result = mixing(subpopulation)
+        result = mixing(self.subpopulation)
 
         self.assertEqual(len(result), 4)
         self.assertEqual(result[0].fitness_, 0.92)
@@ -67,7 +66,7 @@ class TestMixingModel(unittest.TestCase):
 
         random_state = 1
         mixing = mixing_model.NRandom(rule_amount, random_state)
-        result = mixing(subpopulation)
+        result = mixing(self.subpopulation)
 
         self.assertEqual(len(result), 4)
         self.assertEqual(result[0].fitness_, 0.84)
@@ -75,14 +74,12 @@ class TestMixingModel(unittest.TestCase):
         self.assertEqual(result[2].fitness_, 0.58)
         self.assertEqual(result[3].fitness_, 0.66)
 
-    def test_RouletteWheel_filter_subpopulation(self):
+    def test_RouletteWheel(self):
         rule_amount = 4
         random_state = 42
 
-        subpopulation = self.create_subpopulation()
-
         mixing = mixing_model.RouletteWheel(rule_amount, random_state)
-        result = mixing(subpopulation)
+        result = mixing(self.subpopulation)
 
         self.assertEqual(len(result), 4)
         self.assertEqual(result[0].fitness_, 0.74)
@@ -92,10 +89,42 @@ class TestMixingModel(unittest.TestCase):
 
         random_state = 1
         mixing = mixing_model.RouletteWheel(rule_amount, random_state)
-        result = mixing(subpopulation)
+        result = mixing(self.subpopulation)
 
         self.assertEqual(len(result), 4)
         self.assertEqual(result[0].fitness_, 0.93)
         self.assertEqual(result[1].fitness_, 0.66)
         self.assertEqual(result[2].fitness_, 0.99)
         self.assertEqual(result[3].fitness_, 0.13)
+
+    def test_ExperienceCalculation(self):
+        experience_calc = mixing_model.ExperienceCalculation()
+        result_experiences = sorted(experience_calc(self.subpopulation))
+
+        for i in range(100):
+            self.assertEqual(result_experiences[i], i)
+
+    def test_CapExperience(self):
+        for i in range(100):
+            experience_calc = mixing_model.CapExperience(i/2, i)
+            result_experiences = experience_calc(self.subpopulation)
+            self.assertEqual(max(result_experiences), i)
+            self.assertEqual(min(result_experiences), i/2)
+
+    def test_CapExperienceWithDimensionality(self):
+        lower = 5
+        upper = 20
+        experience_calc = mixing_model.CapExperienceWithDimensionality(
+            lower, upper)
+
+        result_experiences = experience_calc(self.subpopulation, 2)
+        self.assertEqual(max(result_experiences), 40)
+        self.assertEqual(min(result_experiences), 10)
+
+        result_experiences = experience_calc(self.subpopulation, 4)
+        self.assertEqual(max(result_experiences), 80)
+        self.assertEqual(min(result_experiences), 20)
+
+        result_experiences = experience_calc(self.subpopulation, 6)
+        self.assertEqual(max(result_experiences), 99)
+        self.assertEqual(min(result_experiences), 30)
