a795b70938cf5dbcc7ed7b84639d90424cd79f6b
RomanSraj <roman.sraj@outlook.com>
2023-08-04 12:47:05 +0200

    Change default for alpha in VolumeRuleFitness to None

--- suprb/rule/fitness.py
+++ suprb/rule/fitness.py
@@ -24,7 +24,7 @@ class VolumeRuleFitness(RuleFitness, metaclass=ABCMeta):
 
     fitness_func_: Callable
 
-    def __init__(self, alpha: float = 0.85):
+    def __init__(self, alpha: float = None):
         self.alpha = alpha
 
     def __call__(self, rule: Rule) -> float:
