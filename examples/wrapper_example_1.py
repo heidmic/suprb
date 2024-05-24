2af04212216ffd6a01160167f50ee01e62b2b36c
Roman-Alexander Sraj <45732695+RomanSraj@users.noreply.github.com>
2024-05-24 15:00:52 +0200

    Add SupRBWrapper class (#157)
    * Add SupRBWrapper to be able to use flat parameters
    
    * Add description to SupRBWrapper

---
+++ examples/wrapper_example_1.py
@@ -0,0 +1,75 @@
+import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import cross_validate, train_test_split
+
+
+from suprb import SupRB
+from suprb.utils import check_random_state
+from suprb.optimizer.rule.es import ES1xLambda
+from suprb.optimizer.solution.ga import GeneticAlgorithm
+from suprb.wrapper import SupRBWrapper
+
+def load_higdon_gramacy_lee(n_samples=1000, noise=0, random_state=None):
+    random_state_ = check_random_state(random_state)
+
+    X = np.linspace(0, 20, num=n_samples)
+    y = np.zeros(n_samples)
+
+    y[X < 10] = np.sin(np.pi * X[X < 10] / 5) + 0.2 * np.cos(4 * np.pi * X[X < 10] / 5)
+    y[X >= 10] = X[X >= 10] / 10 - 1
+
+    y += random_state_.normal(scale=noise, size=n_samples)
+    X = X.reshape((-1, 1))
+
+    return sklearn.utils.shuffle(X, y, random_state=random_state)
+
+def create_plot(scores):
+    fig, axes = plt.subplots(2, 2)
+    X_plot = np.linspace(X.min(), X.max(), 500).reshape((-1, 1))
+    for ax, model in zip(axes.flatten(), scores['estimator']):
+        pred = model.predict(X_plot)
+        ax.scatter(X, y, c='b', s=3, label='y_true')
+        ax.plot(X_plot, pred, c='r', label='y_pred')
+
+    plt.savefig('result.png')
+
+
+if __name__ == '__main__':
+    random_state = 42
+    
+    X, y = load_higdon_gramacy_lee(noise=0.1, random_state=random_state)
+
+    X = MinMaxScaler(feature_range=(-1, 1)).fit_transform(X)
+    y = StandardScaler().fit_transform(y.reshape((-1, 1))).reshape((-1,))
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=random_state)
+
+    # Comparable with examples/example_2.py
+    model = SupRBWrapper(print_config=True,
+                         
+                        ## RULE GENERATION ##
+                        rule_generation=ES1xLambda(), 
+                        rule_generation__n_iter=10, 
+                        rule_generation__lmbda=16, 
+                        rule_generation__operator='+', 
+                        rule_generation__delay=150, 
+                        rule_generation__random_state=random_state, 
+                        rule_generation__n_jobs=1, 
+
+                        ## SOLUTION COMPOSITION ##
+                        solution_composition=GeneticAlgorithm(),
+                        solution_composition__n_iter=32,
+                        solution_composition__population_size=32,
+                        solution_composition__elitist_ratio=0.2,
+                        solution_composition__random_state=random_state,
+                        solution_composition__n_jobs=1)
+
+    scores = cross_validate(model, X, y, cv=4, n_jobs=1, verbose=10,
+                            scoring=['r2', 'neg_mean_squared_error'],
+                            return_estimator=True, fit_params={'cleanup': True})
+
+
+    create_plot(scores)
---
+++ suprb/wrapper.py
@@ -0,0 +1,55 @@
+import warnings
+
+from suprb import SupRB
+
+"""
+This is a wrapper class for SupRB, where parameters can be set without the need to create objects from deep within suprb.
+
+An instance of SupRB gets instantiated with its default values. Depending on the parameters that are given to SupRBWrapper
+the SupRB instance parameters are adapted. This way one can only set the parameters that they want to change, without the 
+necessity to create the whole object. 
+
+There are two possible ways to set parameters using SupRBWrapper (the below examples result in the same configuration):
+
+1) Have a separate parameter for each nested parameter:
+    - solution_composition__crossover=suprb.optimizer.solution.ga.crossover.NPoint()
+    - solution_composition__crossover__crossover_rate=0.91
+    - solution_composition__crossover__n=3
+
+2) Create an object for a nested parameter and pass that object to the SuprbWrapper:
+    - solution_composition__crossover=suprb.optimizer.solution.ga.crossover.NPoint(crossover_rate=0.91, n=3)
+
+SupRBWrapper has some sanity checks to make sure that the parameter name is in fact part of SupRB. If it is not, it will issue a warning.
+
+There is also the possibility to print the final configuration of the SupRB instance using the parameter print_config
+"""
+class SupRBWrapper():
+    def __new__(self, **kwargs):
+        self.suprb = SupRB()
+        
+        kwargs = dict(sorted(kwargs.items(), key=lambda item: item[0].count("__")))
+        
+        for key, value in kwargs.items():
+            if "print_config" == key:
+                continue
+
+            attribute_string = key.replace("__", ".")
+            attribute_split = attribute_string.split(".")
+            attribute_value = self.suprb
+
+            for attribute in attribute_split[:-1]:
+                if hasattr(attribute_value, attribute):
+                    attribute_value = getattr(attribute_value, attribute)
+                
+            if hasattr(attribute_value, attribute_split[-1]):
+                setattr(attribute_value, attribute_split[-1], value)
+            else:
+                warning_text = "The config has conflicting parameters!"
+                warning_text += f"\n{attribute_string} is not part of the config and can have negative effects on the execution!\n\n"
+                warnings.warn(warning_text)
+
+        if "print_config" in kwargs and kwargs["print_config"]:
+            for attr_name, attr_value in self.suprb.__dict__.items():
+                print(attr_name, attr_value)
+
+        return self.suprb
