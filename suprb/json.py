87b28ba7a4a3558519434da92e10ef9cf24b879a
RomanSraj <roman.sraj@outlook.com>
2024-09-11 14:36:58 +0200

    Uncommit necessary line

--- suprb/json.py
+++ suprb/json.py
@@ -34,7 +34,7 @@ def dump(suprb, filename):
     _save_elitist(suprb, json_config)
     _save_config(suprb, json_config)
     _save_pool(suprb.pool_, json_config)
-    # _save_input_space(suprb.pool_, json_config)
+    _save_input_space(suprb.pool_, json_config)
 
     with open(filename, "w") as f:
         json.dump(json_config, f)
