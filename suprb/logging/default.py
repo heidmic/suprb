015949b1ee783e3c7bcc96bedcca3c803fb4f84b
Roman-Alexander Sraj <45732695+RomanSraj@users.noreply.github.com>
2025-01-22 17:36:37 +0100

    Set DefaultLogger as the default logger of support instead of None (#172)
    * Set DefaultLogger as the default logger of support instead of None
    
    ---------
    
    Co-authored-by: heidmic <61311706+heidmic@users.noreply.github.com>

--- suprb/logging/default.py
+++ suprb/logging/default.py
@@ -4,8 +4,8 @@ import json
 import numpy as np
 
 from . import BaseLogger
-from .metrics import matched_training_samples, genome_diversity
-from .. import json as suprb_json
+# from .metrics import matched_training_samples, genome_diversity
+# from .. import json as suprb_json
 from suprb.base import BaseRegressor
 
 
@@ -44,7 +44,7 @@ class DefaultLogger(BaseLogger):
         # Log pool
         pool = estimator.pool_
         log_metric("pool_size", len(pool))
-        log_metric("pool_matched", matched_training_samples(pool))
+        # log_metric("pool_matched", matched_training_samples(pool))  # When using default logging, not all approaches are compatible with this
         if pool:
             log_metric_min_max_mean("pool_fitness", 'fitness_', pool)
 
@@ -52,7 +52,7 @@ class DefaultLogger(BaseLogger):
         # Note that this technically is `PopulationBasedSolutionComposition` specific.
         population = estimator.solution_composition_.population_
         log_metric("population_size", len(population))
-        log_metric("population_diversity", genome_diversity(population))
+        # log_metric("population_diversity", genome_diversity(population))  # When using default logging, not all approaches are compatible with this
         log_metric_min_max_mean("population_fitness", 'fitness_', population)
         log_metric_min_max_mean("population_error", 'error_', population)
         log_metric_min_max_mean("population_complexity", 'complexity_', population)
@@ -62,7 +62,7 @@ class DefaultLogger(BaseLogger):
         log_metric("elitist_fitness", elitist.fitness_)
         log_metric("elitist_error", elitist.error_)
         log_metric("elitist_complexity", elitist.complexity_)
-        log_metric("elitist_matched", matched_training_samples(elitist.subpopulation))
+        # log_metric("elitist_matched", matched_training_samples(elitist.subpopulation))  # When using default logging, not all approaches are compatible with this
         # log_metric("elitist_rules", elitist.pool)
 
         # Log performance
@@ -70,7 +70,7 @@ class DefaultLogger(BaseLogger):
 
     def get_elitist(self, estimator: BaseRegressor):
         json_data = {}
-        suprb_json._save_pool(estimator.solution_composition_.elitist().pool, json_data)
+        # suprb_json._save_pool(estimator.solution_composition_.elitist().pool, json_data)
         return json_data
 
     def log_final(self, X: np.ndarray, y: np.ndarray, estimator: BaseRegressor):
