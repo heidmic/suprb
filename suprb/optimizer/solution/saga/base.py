7091e24712f7464c9d77cdddf4c0d8805117e7dd
RomanSraj <roman.sraj@outlook.com>
2025-03-18 16:28:05 +0100

    Make code more readable

--- suprb/optimizer/solution/saga/base.py
+++ suprb/optimizer/solution/saga/base.py
@@ -91,27 +91,18 @@ class SelfAdaptingGeneticAlgorithmBase(PopulationBasedSolutionComposition):
         pass
 
     def crossover_children(self, parent_pairs, crossover_rate):
-        return list(
-            flatten(
-                [
-                    (
-                        self.crossover(A, B, crossover_rate=crossover_rate, random_state=self.random_state_),
-                        self.crossover(B, A, crossover_rate=crossover_rate, random_state=self.random_state_),
-                    )
-                    for A, B in parent_pairs
-                ]
-            )
-        )
+        # fmt: off
+        return list(flatten(
+            [(self.crossover(A, B, crossover_rate, self.random_state_), self.crossover(B, A, crossover_rate, self.random_state_)) 
+             for A, B in parent_pairs
+        ]))
+        # fmt: on
 
     def mutate_children(self, children, X=None, y=None):
         return [self.mutation(child, self.mutation_rate, random_state=self.random_state_) for child in children]
 
     def parent_selection(self):
-        return self.selection(
-            population=self.population_,
-            n=self.population_size,
-            random_state=self.random_state_,
-        )
+        return self.selection(population=self.population_, n=self.population_size, random_state=self.random_state_)
 
     def _optimize(self, X: np.ndarray, y: np.ndarray):
         assert self.population_size % 2 == 0
@@ -121,28 +112,16 @@ class SelfAdaptingGeneticAlgorithmBase(PopulationBasedSolutionComposition):
 
         for _ in range(self.n_iter):
             self.fitness_calculation()
-            # Adjust Rates for current fitness
             self.update_genetic_operator_rates()
-
-            # Eltitism
             elitists = sorted(self.population_, key=lambda i: i.fitness_, reverse=True)[: self.n_elitists_]
-
-            # Selection
             parents = self.parent_selection()
 
             # Note that this expression swallows the last element, if `population_size` is odd
             parent_pairs = map(lambda *x: x, *([iter(parents)] * 2))
 
-            # Crossover
-            try:
-                children = self.crossover_children(parent_pairs)
-            except TypeError:
-                children = self.crossover_children(parent_pairs, self.crossover_rate)
-
-            # Mutation
+            children = self.crossover_children(parent_pairs, self.crossover_rate)
             mutated_children = self.mutate_children(children, X, y)
 
-            # Replacement
             self.population_ = elitists
             self.population_.extend(mutated_children)
 
@@ -304,129 +283,66 @@ class SelfAdaptingGeneticAlgorithm2(SelfAdaptingGeneticAlgorithmBase):
         self.fitness_min = np.min([i.fitness_ for i in self.population_])
         self.fitness_max = np.max([i.fitness_ for i in self.population_])
 
-    def mutate_func(
-        self,
-        solution: Solution,
-        mutation_rate_min: float,
-        mutation_rate_max: float,
-        fitness_mean: float,
-        fitness_max: float,
-        random_state: RandomState,
-    ) -> Solution:
-        if fitness_max == fitness_mean:
-            mutation_rate = mutation_rate_min
-        elif solution.fitness_ > fitness_mean:
-            mutation_rate = mutation_rate_min + (mutation_rate_max - mutation_rate_min) * (
-                (fitness_max - solution.fitness_) / (fitness_max - fitness_mean)
+    def mutate_func(self, solution: Solution) -> Solution:
+        if self.fitness_max == self.fitness_mean:
+            mutation_rate = self.mutation_rate_min
+        elif solution.fitness_ > self.fitness_mean:
+            mutation_rate = self.mutation_rate_min + (self.mutation_rate_current_max - self.mutation_rate_min) * (
+                (self.fitness_max - solution.fitness_) / (self.fitness_max - self.fitness_mean)
             )
         else:
-            mutation_rate = mutation_rate_max
+            mutation_rate = self.mutation_rate_current_max
 
-        bit_flips = random_state.random(solution.genome.shape) < mutation_rate
+        bit_flips = self.random_state_.random(solution.genome.shape) < mutation_rate
         genome = np.logical_xor(solution.genome, bit_flips)
 
         return solution.clone(genome=genome)
-
-    def crossover_func(
-        self,
-        A: Solution,
-        B: Solution,
-        crossover_rate_min,
-        crossover_rate_max,
-        fitness_mean,
-        fitness_min,
-        fitness_max,
-        random_state: RandomState,
-    ) -> Solution:
-        fitness_parents_mean = (A.fitness_ + B.fitness_) / 2
-
-        crossover_rate_diff = crossover_rate_max - crossover_rate_min
-        fitness_diff = abs(fitness_mean - fitness_parents_mean)
-
-        if fitness_mean in {fitness_min, fitness_max}:
-            crossover_rate = crossover_rate_max
-        elif fitness_parents_mean <= fitness_mean:
-            crossover_rate = crossover_rate_max - crossover_rate_diff * (fitness_diff / (fitness_mean - fitness_min))
-        else:
-            crossover_rate = crossover_rate_max - crossover_rate_diff * (fitness_diff / (fitness_max - fitness_mean))
-
-        if random_state.random() < crossover_rate:
-            return self.crossover._crossover(A=A, B=B, random_state=random_state)
-
-        return A
-
-    def crossover_children(self, parent_pairs):
-        return list(
-            flatten(
-                [
-                    (
-                        self.crossover_func(
-                            A,
-                            B,
-                            self.crossover_rate_current_min,
-                            self.crossover_rate_max,
-                            self.fitness_mean,
-                            self.fitness_min,
-                            self.fitness_max,
-                            random_state=self.random_state_,
-                        ),
-                        self.crossover_func(
-                            B,
-                            A,
-                            self.crossover_rate_current_min,
-                            self.crossover_rate_max,
-                            self.fitness_mean,
-                            self.fitness_min,
-                            self.fitness_max,
-                            random_state=self.random_state_,
-                        ),
-                    )
-                    for A, B in parent_pairs
-                ]
-            )
-        )
 
     def mutate_children(self, children, X, y):
         for child in children:
             child.fit(X, y)
 
-        mutated_children = [
-            self.mutate_func(
-                child,
-                self.mutation_rate_min,
-                self.mutation_rate_current_max,
-                self.fitness_mean,
-                self.fitness_max,
-                random_state=self.random_state_,
-            )
-            for child in children
-        ]
+        mutated_children = [self.mutate_func(child) for child in children]
 
         return mutated_children
 
+    def crossover_func(self, A: Solution, B: Solution) -> Solution:
+        fitness_parents_mean = (A.fitness_ + B.fitness_) / 2
+
+        rate_diff = self.crossover_rate_max - self.crossover_rate_min
+        fitness_diff = abs(self.fitness_mean - fitness_parents_mean)
+
+        if self.fitness_mean in {self.fitness_min, self.fitness_max}:
+            new_rate = self.crossover_rate_max
+        elif fitness_parents_mean <= self.fitness_mean:
+            new_rate = self.crossover_rate_max - rate_diff * (fitness_diff / (self.fitness_mean - self.fitness_min))
+        else:
+            new_rate = self.crossover_rate_max - rate_diff * (fitness_diff / (self.fitness_max - self.fitness_mean))
+
+        if self.random_state_.random() < new_rate:
+            return self.crossover._crossover(A=A, B=B, random_state=self.random_state_)
+
+        return A
+
+    def crossover_children(self, parent_pairs, crossover_rate):
+        return list(flatten([(self.crossover_func(A, B), self.crossover_func(B, A)) for A, B in parent_pairs]))
+
     def calc_similarity(self):
         try:
-            dot_sum = np.sum(
-                [
-                    np.multiply(self.old_gen[i].fitness_, self.population_[i].fitness_)
-                    for i in range(len(self.population_))
-                ]
-            )
+            # fmt: off
+            dot_sum = np.sum([np.multiply(self.old_gen[i].fitness_, self.population_[i].fitness_)
+                                for i in range(len(self.population_))])
+            # fmt: on
+
             length_new = np.sqrt(np.sum([np.square(i.fitness_) for i in self.population_]) + np.exp(-10))
             length_old = np.sqrt(np.sum([np.square(i.fitness_) for i in self.old_gen]) + np.exp(-10))
             cosine_similarity = dot_sum / (length_new + length_old)
-            genome_similarity = (
-                np.intersect1d(
-                    [i.fitness_ for i in self.population_],
-                    [i.fitness_ for i in self.old_gen],
-                ).size
-                / np.union1d(
-                    [i.fitness_ for i in self.population_],
-                    [i.fitness_ for i in self.old_gen],
-                ).size
-            )
+            intersect_1d = np.intersect1d([i.fitness_ for i in self.population_], [i.fitness_ for i in self.old_gen])
+            union_1d = np.union1d([i.fitness_ for i in self.population_], [i.fitness_ for i in self.old_gen])
+            genome_similarity = intersect_1d.size / union_1d.size
         except ZeroDivisionError:
             return 1
+
         return cosine_similarity * genome_similarity
 
     def calc_diversity(self):
@@ -435,11 +351,14 @@ class SelfAdaptingGeneticAlgorithm2(SelfAdaptingGeneticAlgorithmBase):
             self.fitness_variance_max = fitness_variance
         if fitness_variance < self.fitness_variance_min:
             self.fitness_variance_min = fitness_variance
+
         a = (fitness_variance - self.fitness_variance_min) / self.fitness_variance_max
+
         if self.fitness_max == self.fitness_min:
             b = 0
         else:
             b = np.cos(np.pi / 2 * (1 - (self.fitness_mean - self.fitness_min) / (self.fitness_max - self.fitness_min)))
+
         return a * b
 
     def calc_quality(self):
@@ -447,14 +366,11 @@ class SelfAdaptingGeneticAlgorithm2(SelfAdaptingGeneticAlgorithmBase):
 
     def update_genetic_operator_rates(self):
         quality = self.calc_quality()
-        # crossover rate
-        self.crossover_rate_current_min = (
-            self.crossover_rate_min + quality * (self.crossover_rate_max - self.crossover_rate_min) / 2
-        )
-        # mutationrate
-        self.mutation_rate_current_max = (
-            self.mutation_rate_max - quality * (self.mutation_rate_max - self.mutation_rate_min) / 2
-        )
+
+        # fmt: off
+        self.crossover_rate_current_min = (self.crossover_rate_min + quality * (self.crossover_rate_max - self.crossover_rate_min) / 2)
+        self.mutation_rate_current_max = (self.mutation_rate_max - quality * (self.mutation_rate_max - self.mutation_rate_min) / 2)
+        # fmt: on
 
 
 class SelfAdaptingGeneticAlgorithm3(SelfAdaptingGeneticAlgorithmBase):
@@ -501,18 +417,13 @@ class SelfAdaptingGeneticAlgorithm3(SelfAdaptingGeneticAlgorithmBase):
 
         self.parameter_mutation_rate = parameter_mutation_rate
 
-    def crossover_children(self, parent_pairs):
-        return list(
-            flatten(
-                [
-                    (
-                        self.crossover(A, B, random_state=self.random_state_),
-                        self.crossover(B, A, random_state=self.random_state_),
-                    )
-                    for A, B in parent_pairs
-                ]
-            )
-        )
+    def crossover_children(self, parent_pairs, crossover_rate):
+        # fmt: off
+        return list(flatten(
+            [(self.crossover(A, B, random_state=self.random_state_), self.crossover(B, A, random_state=self.random_state_)) 
+             for A, B in parent_pairs
+        ]))
+        # fmt: on
 
     def mutate_children(self, children, X=None, y=None):
         return children
