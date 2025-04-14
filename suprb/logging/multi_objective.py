12f4458489c760d78306918e16ec01f145a4ffb6
felix <felix@heptner.de>
2025-04-14 11:42:36 +0200

    Log Pareto Front every SC Phase

--- suprb/logging/multi_objective.py
+++ suprb/logging/multi_objective.py
@@ -9,11 +9,10 @@ from .metrics import hypervolume, spread
 
 class MOLogger(DefaultLogger):
 
-    pareto_front_: list[Solution]
+    pareto_front_: list[list[Solution]] = []
 
-    def log_final(self, X: np.ndarray, y: np.ndarray, estimator: BaseRegressor):
-        super().log_final(X, y, estimator)
-        self.pareto_front_ = estimator.solution_composition_.pareto_front()
-        self.metrics_["hypervolume"] = hypervolume(self.pareto_front_)
-        self.metrics_["spread"] = spread(self.pareto_front_)
-        return
+    def log_iteration(self, X: np.ndarray, y: np.ndarray, estimator: BaseRegressor, iteration: int):
+        super().log_iteration(X, y, estimator, iteration)
+        self.pareto_front_.append(estimator.solution_composition_.pareto_front())
+        self.log_metric("hypervolume", hypervolume(self.pareto_front_), estimator.step_)
+        self.log_metric("spread", spread(self.pareto_front_), estimator.step_)
