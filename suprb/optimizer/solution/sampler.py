243a174c9507ce0fbee63afa167e2f7227cf2d2a
felix <felix@heptner.de>
2025-04-24 15:20:49 +0200

    Add BetaSolutionSampler
    * Added BetaSolutionSampler which represent a beta distribution
    over the Pareto Front.
    
    * Points can optionally be projected onto the normal vector simplex,
    and onto the x-axis (currently works in only 2 dimensional objective
    functions)
    
    * Removed Uniform Sampler as it can be well approximated by the Normal
    sampler and represented by the new beta sampler.

--- suprb/optimizer/solution/sampler.py
+++ suprb/optimizer/solution/sampler.py
@@ -16,15 +16,32 @@ class SolutionSampler(BaseComponent, metaclass=ABCMeta):
         pass
 
 
-class UniformSolutionSampler(SolutionSampler):
-
-    def __call__(self, pareto_front: list[Solution], random_state: RandomState) -> Solution:
-        return random_state.choice(pareto_front)
-
-
 class NormalSolutionSampler(SolutionSampler):
 
-    def __call__(self, pareto_front: list[Solution], random_state: RandomState, mu: float = 5.0) -> Solution:
-        weights = stats.norm.pdf(np.linspace(0, len(pareto_front), len(pareto_front)), len(pareto_front) / 2, mu)
+    def __init__(self, mu: float = 0.0):
+        self.mu = mu
+
+    def __call__(self, pareto_front: list[Solution], random_state: RandomState) -> Solution:
+        weights = stats.norm.pdf(np.linspace(0, 1, len(pareto_front)), 0.5, self.mu)
+        weights = weights / np.sum(weights)
+        return random_state.choice(pareto_front, p=weights)
+
+
+class BetaSolutionSampler(SolutionSampler):
+
+    def __init__(self, alpha: float = 1.0, beta: float = 1.0, projected: bool = True):
+        self.alpha = alpha
+        self.beta = beta
+        self.projected = projected
+
+    def __call__(self, pareto_front: list[Solution], random_state: RandomState) -> Solution:
+        if self.projected:
+            points = np.array([solution.fitness_ for solution in pareto_front])
+            points = points / np.sum(points, axis=1)
+            points = points[:, 0]
+        else:
+            points = np.linspace(0, 1, len(pareto_front))
+
+        weights = stats.norm.beta(points, self.alpha, self.beta)
         weights = weights / np.sum(weights)
         return random_state.choice(pareto_front, p=weights)
