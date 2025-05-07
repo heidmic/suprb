8ad477cb5a5a3e47252ab4ff340796e003e9f909
felix <felix@heptner.de>
2025-05-07 09:52:58 +0200

    Add Warm Start to TSSC

--- suprb/optimizer/solution/ts/base.py
+++ suprb/optimizer/solution/ts/base.py
@@ -6,7 +6,7 @@ from suprb import Solution
 from suprb.solution import SolutionInit
 from suprb.solution.initialization import RandomInit
 from ..base import SolutionComposition, SolutionArchive, MOSolutionComposition, PopulationBasedSolutionComposition
-
+from suprb.utils import check_random_state
 
 class TwoStageSolutionComposition(SolutionComposition):
     """
@@ -14,10 +14,12 @@ class TwoStageSolutionComposition(SolutionComposition):
 
     Parameters
     ----------
-    algorithms: list[SolutionComposition]
-        List of solution composition algorithms to be used.
-    n_iters: list[int]
-        Number of iterations for each algorithm.
+    algorithm_1: PopulationBasedSolutionComposition
+        Algorithm to be used in the first stage.
+    algorithm_2: PopulationBasedSolutionComposition
+        Algorithm to be used in the second stage.
+    switch_iteration: int
+        Iteration at which to switch from algorithm_1 to algorithm_2.
 
     """
 
@@ -31,7 +33,7 @@ class TwoStageSolutionComposition(SolutionComposition):
         archive: SolutionArchive = None,
         random_state: int = None,
         n_jobs: int = None,
-        warm_start: bool = None,
+        warm_start: bool = True,
     ):
         super().__init__(
             n_iter=n_iter, init=init, archive=archive, random_state=random_state, n_jobs=n_jobs, warm_start=warm_start
@@ -51,8 +53,21 @@ class TwoStageSolutionComposition(SolutionComposition):
             self.algorithm_1.init.fitness.max_genome_length_ = self.init.fitness.max_genome_length_
             self.algorithm_2.init.fitness.max_genome_length_ = self.init.fitness.max_genome_length_
 
+        self.algorithm_1.random_state = self.random_state
+        self.algorithm_2.random_state = self.random_state
+        self.random_state_ = check_random_state(self.random_state)
+
         if self.step_ == self.switch_iteration - 1:
             self.current_algo_ = self.algorithm_2
+            if self.warm_start:
+                pop = []
+                algo_2_fitness = self.algorithm_2.init.fitness
+                for solution in self.algorithm_1.population_:
+                    solution.fitness = algo_2_fitness
+                    solution = self.init.pad(solution, self.random_state_)
+                    solution.fit(X, y)
+                    pop.append(solution)
+                self.algorithm_2.population_ = pop
 
         self.current_algo_.random_state = self.random_state
         self.current_algo_.optimize(X, y)
